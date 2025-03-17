83be0b8f9093bc0dc1d0057f1043c2e07f13e601
BrittanyC <carvajalbrittany13@gmail.com>
2025-03-16 23:27:10 -0600

    edicion de funciones

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function validadEntrada(){
     // Si el campo está vacío, mostramos una alerta y salimos de la función.
     if (NomAmigo === "") {
         alert("⚠️ Ingresa un nombre antes de agregar.");
-        return; // Salimos de la función si no hay nombre.
+        return null; // Salimos de la función si no hay nombre.
     }
 
     console.log("Amigo agregado:", NomAmigo); // Mostramos en la consola el nombre ingresado
@@ -21,18 +21,20 @@ function agregarAmigo(){ //Agrega el nombre del amigo a la lista
 
     console.log(amigoslista)
 
-    // Limpiar el campo de texto después de agregar el nombre
-    document.getElementById("amigo").value = "";
-
     let listaNomHTML = document.getElementById("name-list");
     listaNomHTML.innerHTML = amigoslista;
 
     return amigoslista;
 }
 
+function limpiarCampo(){
+    // Limpiar el campo de texto después de agregar el nombre
+    document.getElementById("amigo").value = "";
+}
+
 function sortearAmigo(){
     let amigoSorteado = Math.random(amigoslista);
-    
+
     console.log(amigoSorteado)
 
     return amigoSorteado;
