f70b47b0b8b190baba8ed8670000549c861f3061
BrittanyCarvajalD <142198918+BrittanyCarvajalD@users.noreply.github.com>
2025-03-16 22:03:37 -0600

    Edicion de app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+ let NomAmigo = document.getElementById("amigo");
+ return NomAmigo;
+}
+
+console.log(agregarAmigo());
+
+function sortearAmigo(){
+
+}
